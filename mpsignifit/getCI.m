95cf3ee502cc8ed34f8a4e679732d605aef8b36a
mani33 <manivannan33@yahoo.com>
2013-01-29 10:23:09 -0600

    Removed parseVarArgs function dependency

--- mpsignifit/getCI.m
+++ mpsignifit/getCI.m
@@ -1,5 +1,5 @@
-function ci = getCI ( inference, cut, p,varargin )
-% ci = getCI ( inference, cut )
+function ci = getCI ( inference, cut, p, param, bca )
+% ci = getCI ( inference, cut, p, param, bca )
 %
 % Get the confidence interval from the inference object
 %
@@ -9,29 +9,38 @@ function ci = getCI ( inference, cut, p,varargin )
 %
 % p should be the desired coverage of the confidence interval (p<1)
 %
-%
+% 'param' should be 'slope' or 'threshold' for which we need the CI.
+% 'bca' set to true to use Bias Corrected and Accelerated CI estimation.
+
 % This file is part of psignifit3 for matlab (c) by Ingo Fründ
 % 
-% example:   getCI(fitData, 1, 0.95, 'param', 'slope')
-%            getCI(fitData, 1, 0.95, 'param', 'threshold','bca',false)
+% example:   getCI(fitData, 1, 0.95, 'slope')
+%            getCI(fitData, 1, 0.95, 'threshold', true)
 %
-% Modified by MS 2013-01-29
+%
+% Modified by MS 2013-01-30
 
+% Default values
+if nargin < 4
+    param = 'threshold';
+    bca = true;
+end
 
-args.param = 'threshold'; % 'slope' or 'threshold' for which we need the CI
-args.bca = true; % should use Bias Corrected and Accelerated CI estimation?
-args = parseVarArgs(args,varargin{:});
+if nargin < 5
+    bca = true;
+end
+
 
 notin = 1-p;
 probs = [0.5*notin,1-0.5*notin];
 
-if args.bca && strcmp ( inference.call, 'bootstrap' )
+if bca && strcmp ( inference.call, 'bootstrap' )
     bias = inference.bias_thres(cut);
     acc  = inference.acc_thres(cut);
     probs = normcdf( bias + ( norminv(probs) + bias ) ./ (1-acc*(norminv(probs) + bias )) );
 end;
 
-switch args.param
+switch param
     case 'threshold'
         ci = prctile ( inference.mcthres(:,cut), 100*probs );
     case 'slope'
